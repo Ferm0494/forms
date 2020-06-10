cac01c6f17e016d9b2fc32f24b16ae1155a0c8f8
Ferm0494 <ferm0494@gmail.com>
2020-06-10 12:06:53 -0600

    adding edit and forms_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,33 @@ class UsersController < ApplicationController
   def new
     @user= User.new
   end
-  def create 
-    @user= User.new(user_params)
 
-    if @user.save
-      redirect_to new_user_path
+  def create 
+    @user = User.new(user_params)
+  if @user.save
+    flash[:notice]= "User created!"
+    redirect_to new_user_path
+  else
+    flash[:notice]= "Please fix the errors : "
+    render 'new'
+  end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params) 
+        flash[:notice]= "User updated correctly!"
+        redirect_to edit_user_path
     else
-      render "new"
+        flash[:notice]= "Fix the errors"
+        render 'edit'
     end
+
   end
     
   private
